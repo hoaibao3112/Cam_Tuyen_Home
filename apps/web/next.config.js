fe942080d30a27addd57b80507521887b9a45001
hoaibao3112 <baohoaitran3112@gmail.com>
2026-06-29 10:52:43 +0700

    fix(web): allow wildcard Supabase host for image remotePatterns

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'vatucnvyteqdpjjdxgwn.supabase.co',
+        hostname: '*.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
